9cd5b9a626caa757ea8090e270748f5adfba9d17
syrenetidelacruz <syrenetidelacruz4@gmail.com>
2021-09-03 14:31:13 -0400

    XML Done

---
+++ app/src/main/java/com/example/mobilep1/ScoreViewModel.java
@@ -0,0 +1,9 @@
+package com.example.mobilep1;
+
+import androidx.lifecycle.ViewModel;
+
+public class ScoreViewModel extends ViewModel {
+    public int teamAScore = 0; //tracks Team A Score
+
+    public int teamBScore = 0; //tracks Team B Score
+}
