4d2caab1bb1281e1a596afa7e76f704b5de28194
syrenetidelacruz <syrenetidelacruz4@gmail.com>
2021-09-09 11:01:10 -0400

    Landscape XML

---
+++ app/src/main/java/com/example/mobilep1/ScoreView.kt
@@ -0,0 +1,12 @@
+package com.example.mobilep1
+
+import android.widget.TextView
+import androidx.lifecycle.ViewModel
+import android.content.res.Configuration
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScoreView: ViewModel() {
+    var teamAScore = 0
+    var teamBScore = 0
+}
